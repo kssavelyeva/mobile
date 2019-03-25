181c0b01ff478ef97fec850da38d26f6e6563139
Карина <tinkOFFSKS4tAsk>
2019-03-25 13:35:49 +0300

    opera

--- src/test/java/BrowsersFactory.java
+++ src/test/java/BrowsersFactory.java
@@ -1,13 +1,10 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
-
-
-
-import java.io.File;
-
-import static org.junit.Assume.assumeTrue;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.opera.OperaOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public enum BrowsersFactory {
     chrome {
@@ -21,7 +18,23 @@ public enum BrowsersFactory {
             updateProperty("firefox");
             return new FirefoxDriver();
         }
-    };
+    },
+    opera {
+        public WebDriver create(){
+            System.setProperty("webdriver.opera.driver","C:\\Users\\Карина\\Desktop\\operadriver_win64\\operadriver.exe");
+            OperaOptions options = new OperaOptions();
+            options.setBinary("C:\\Users\\Карина\\AppData\\Local\\Programs\\Opera\\58.0.3135.118_0\\opera.exe");
+            options.setExperimentalOption("useAutomationExtension", false);
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+            updateProperty("opera");
+            return new OperaDriver(capabilities);
+        }
+    }
+    ;
 
     public WebDriver create(){
         return null;
